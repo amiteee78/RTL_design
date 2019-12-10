68479f68d043ade15b5bda1206b48af0605c3f17
ashuvo <amiteee78@gmail.com>
2019-12-10 23:32:56 +0600

    unnecessary directory removed.

--- apb2apb_bridge/apb_verilog/run/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1543821468 /home/ashuvo/amit/fsm/apb_interface/rtl/apb_master.v
-1543821091 /home/ashuvo/amit/fsm/apb_interface/tb/apb_intf_tb.sv
-1543749716 /home/ashuvo/amit/fsm/apb_interface/rtl/apb_slave.v
-1543827747 /home/ashuvo/amit/fsm/apb_interface/rtl/apb_interface.v
--- apb2apb_bridge/apb_verilog/run/wave_view.sh
+++
@@ -1,1 +0,0 @@
-simvision wave_database/$1.shm/$1.trn -input wave_database/$1.svcf &
